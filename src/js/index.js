b63bde0e5943846051a4ed5d59127e77f50d09b6
arfmlna <arima06917@gmail.com>
2024-04-10 22:09:38 +0700

    UTS update

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 const navlinkmobile = document.getElementById('nav-link-mobile')
 const navbtn = document.getElementsByClassName('nav-btn')[0]
 const aweseomeBar = document.getElementsByClassName('icon-bars')[0]
-const audio = document.getElementById('audio')
 const loading = document.getElementById('loading')
 const btnMore = document.getElementById('btn-more')
 const btnClose = document.getElementById('btn-close')
 const dialog = document.getElementById('dialog')
-
 
 setTimeout(() => {
     loading.style.display = 'none'
@@ -22,14 +20,6 @@ navbtn.addEventListener('click', () => {
     }
 })
 
-btnMore.addEventListener('click', () => {
-    dialog.show()
-})
-
-btnClose.addEventListener('click', () => {
-    dialog.close()
-})
-
 function typewriter(selector, texts, speed, loop) {
     new TypeIt(selector, {
         strings: texts,
@@ -38,7 +28,13 @@ function typewriter(selector, texts, speed, loop) {
     }).go();
 }
 
-typewriter(".load", "Loading...", 100, true)
-
+typewriter(".text-loading", "Loading...", 100, true)
 typewriter(".typewriter", "Hi nama saya Arif Maulana saya adalah seorang mahasiswa dari Universitas Muhammadiyah Tasikmalaya. Saya berada dalam naungan Fakultas Keguruan dan Ilmu Pendidikan(FKIP) dan juga jurusan prodi saya adalah Pendidikan Teknologi Informasi(PTI) yang targetnya untuk menjadi guru dalam bidang teknologi informasi di sekolah.", 75, true)
 
+btnMore.addEventListener('click', () => {
+    dialog.show()
+})
+
+btnClose.addEventListener('click', () => {
+    dialog.close()
+})
